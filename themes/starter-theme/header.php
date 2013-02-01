8ae855ac96b1f6e478c4f7a5a11595f46892f0aa
Thomas J Bradley <theman@thomasjbradley.ca>
2013-01-31 20:24:08 -0500

    Removed some extra stuff I don't use anymore.

--- themes/starter-theme/header.php
+++ themes/starter-theme/header.php
@@ -1,10 +1,5 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html lang="en-ca" class="no-js ie6"><![endif]-->
-<!--[if (IE 7) & !(IEMobile)]><html lang="en-ca" class="no-js ie7"><![endif]-->
-<!--[if (IE 7) & (IEMobile)]><html lang="en-ca" class="no-js ie7 ie7mobile"><![endif]-->
-<!--[if IE 8]><html lang="en-ca" class="no-js ie8"><![endif]-->
-<!--[if IE 9]><html lang="en-ca" class="no-js ie9"><![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html lang="en-ca" class="no-js"><!--<![endif]-->
+<html lang="en-ca" class="no-js">
 <head>
   <meta charset="utf-8">
   <title><?php wp_title(''); ?></title>
